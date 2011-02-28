ea19ca295e30a4f8a7075b5a3f56bcc140e36982
Michal Sitko <pierwszy1@gmail.com>
2011-02-28 22:39:32 +0100

    Node.h added, basic tags parsing works

--- misc/html_tag_name.cpp
+++ misc/html_tag_name.cpp
@@ -7,23 +7,29 @@
 #include <boost/spirit/include/phoenix_core.hpp>
 #include <boost/spirit/include/phoenix_operator.hpp>
 
+#include <boost/bind.hpp>
+
 namespace spirit = boost::spirit;
-using namespace spirit;
 using spirit::qi::raw;
 using spirit::ascii::alpha;
 using spirit::ascii::alnum;
 using spirit::ascii::char_;
+using spirit::no_skip;
+using spirit::omit;
 using spirit::lit;
+using boost::bind;
 namespace fusion = boost::fusion;
-using namespace boost::phoenix;
-using namespace boost::phoenix::arg_names;
+//using namespace boost::phoenix;
+//using namespace boost::phoenix::arg_names;
 
 
 typedef fusion::vector<std::string, std::vector<fusion::vector<std::string, std::string> > > HtmlTagAttr;
 typedef std::vector<HtmlTagAttr> HtmlAttr;
-typedef qi::rule<std::string::iterator, fusion::vector<std::string, std::string>(), ascii::space_type> HtmlAttributeRule;
-typedef qi::rule<std::string::iterator, HtmlTagAttr(), ascii::space_type> HtmlTagRule;
-typedef qi::rule<std::string::iterator, HtmlAttr(), ascii::space_type> HtmlRule;
+typedef spirit::qi::rule<std::string::iterator, fusion::vector<std::string, std::string>(), spirit::ascii::space_type> HtmlAttributeRule;
+typedef spirit::qi::rule<std::string::iterator, HtmlTagAttr(), spirit::ascii::space_type> HtmlTagRule;
+typedef spirit::qi::rule<std::string::iterator, std::string(), spirit::ascii::space_type> HtmlEndTagRule;
+//typedef spirit::qi::rule<std::string::iterator, HtmlAttr(), spirit::ascii::space_type> HtmlRule;
+typedef spirit::qi::rule<std::string::iterator, spirit::ascii::space_type> HtmlRule;
 
 void print_attr(const fusion::vector<std::string, std::string> &v){
 	std::cout << fusion::at_c<0>(v) << std::endl << fusion::at_c<1>(v) << std::endl;
@@ -40,12 +46,14 @@ void print_tag(HtmlTagAttr &tag){
 }
 
 void test_tag(std::string &in, HtmlTagAttr &res){
+	using spirit::ascii::blank;
+	using spirit::ascii::space;
  	HtmlAttributeRule attribute;
  	HtmlTagRule start_tag;
  	attribute = +(char_-'=') >> '=' >> -(lit('\'') | '"')  >> *(char_-(lit('\'') | '"')) >> -(lit('\'') | '"');
- 	start_tag %= no_skip['<' >> +(char_-(ascii::blank | '>' | '/')) >> ascii::blank] >> *(attribute) >> '>';
+ 	start_tag %= no_skip['<' >> +(char_-(blank | '>' | '/')) >> blank] >> *(attribute) >> '>';
  	
- 	qi::phrase_parse(in.begin(), in.end(), start_tag, ascii::space, res);
+ 	spirit::qi::phrase_parse(in.begin(), in.end(), start_tag, space, res);
  	std::cout << fusion::at_c<0>(res) << std::endl;
  	print_attrs(fusion::at_c<1>(res));
 }
@@ -58,24 +66,37 @@ struct Creator{
 	void handle_start_tag(HtmlTagAttr &tag){
 		std::cout << "Beginning of tag: " << fusion::at_c<0>(tag) << std::endl;
 	}
+
+	void handle_end_tag(std::string &tag){
+		std::cout << "End of tag: " << tag << std::endl;
+	}
+
+	void handle_start_end_tag(HtmlTagAttr &tag){
+		std::cout << "Beginning and end of tag: " << fusion::at_c<0>(tag) << std::endl;
+	}
 };
 
 void test_html(std::string &in, HtmlAttr &res){
 	using namespace std;
+	using spirit::ascii::space;
 	HtmlRule html;
 	HtmlAttributeRule attribute;
- 	HtmlTagRule start_tag;
+ 	HtmlTagRule start_tag, start_end_tag;
+	HtmlEndTagRule end_tag;
  	
+	Creator creator;
+
 	attribute = +(char_-(char_('=') | '>' | '<')) >> '=' >> -(lit('\'') | '"')  >> *(char_-(lit('\'') | '"')) >> -(lit('\'') | '"');
- 	start_tag = no_skip['<' >> +(char_-(ascii::space | '/' | '>' | '<'))] >> *(attribute) >> '>';
-	html = *(omit[*(char_-'<')] >> start_tag[&handle_start_tag]);
+ 	start_tag = no_skip['<' >> +(alnum)] >> *(attribute) >> '>';
+	start_end_tag = no_skip['<' >> +(alnum)] >> *(attribute) >> lit("/>");
+	end_tag = no_skip[lit("</") >> +(alnum)] >> '>';
+
+	html = *(omit[*(char_-'<')] >> (start_tag[bind(&Creator::handle_start_tag, &creator, _1)] | end_tag[bind(&Creator::handle_end_tag, &creator, _1)] | start_end_tag[bind(&Creator::handle_start_end_tag, &creator, _1)]));
+//	html = *(omit[*(char_-'<')] >> (start_end_tag[bind(&Creator::handle_start_end_tag, &creator, _1)]));
+	//html = *(omit[*(char_-'<')] >> end_tag);
 //	html = *(omit[*(char_-'<')] >> '<' >> (+(char_-'>'))[&handle_start_tag2] >> '>');
 
-	qi::phrase_parse(in.begin(), in.end(), html, ascii::space, res);
-	cout << res.size() << endl;
-	
-	for(int i=0; i<res.size(); ++i)
-		print_tag(res.at(i));
+	spirit::qi::phrase_parse(in.begin(), in.end(), html, space);
 }
 
 int main(){
@@ -90,6 +111,6 @@ int main(){
 // 	test_tag(in3, res3);
 
 	HtmlAttr out; 
-	in = "<p>fsdafsa<h2 id=\"logo\" class=\"classname\"></h2></p>";
+	in = "<p>fsdafsa<h2 id=\"logo\" class=\"classname\"></h2><img href='example.gif' /></p>";
 	test_html(in, out);
 }
